56537467838472afdf71133600ff43e678370c79
Mrhairui <chenwentao_622@163.com>
2019-06-06 22:10:25 +0800

    TREE

--- tree.py
+++ tree.py
@@ -4,10 +4,31 @@ from sklearn.preprocessing import LabelEncoder
 dataset = pd.read_csv('credit.csv', encoding="gbk")
 X = dataset.iloc[:, :-1].values   # 这样才能是数值
 Y = dataset.iloc[:, -1].values
+XP =  list(dataset.iloc[:, :-1])
+XM = dataset.iloc[:, :-1].values.copy()
+YM = dataset.iloc[:, -1].values.copy()
 labelencoder = LabelEncoder()
 for i in range(0, 4):
     X[:, i] = labelencoder.fit_transform(X[:, i])
+
 Y = labelencoder.fit_transform(Y)
+XMM = {}
+YPP = {}
+
+for i in range(len(X[0])):
+    XMM[i] = {}
+    for j in range(len(Y)):
+        if X[j, i] not in XMM[i].keys():
+            XMM[i][X[j, i]] = XM[j, i]
+for j in range(len(Y)):
+    if Y[j] not in YPP.keys():
+        YPP[Y[j]] = YM[j]
+
+
+
+
+
+
 
 
 def cal_ent(yy):
@@ -71,15 +92,11 @@ def split_data(X, Y, feature):
     return tt, yy
 
 
-
-
-
 def creat_tree(X, Y):
     n = len(X[0])
     m = len(Y)
     if len(set(Y)) == 1:
-        return Y[0]
-    maxi = 0
+        return YPP[Y[0]]
     min_gain = 0
 
     for i in range(n):
@@ -87,10 +104,11 @@ def creat_tree(X, Y):
         if gain_inf > min_gain:
             min_gain = gain_inf
             maxi = i
-    tree = {maxi:{}}
+    tree = {XP[maxi]:{}}
     tt, yy = split_data(X, Y, maxi)
     for key in tt.keys():
-        tree[maxi][key] = creat_tree(np.array(tt[key]) , np.array(yy[key]))
+        tree[XP[maxi]][XMM[maxi][key]] = creat_tree(np.array(tt[key]) , np.array(yy[key]))
+    return tree     # 递归函数的返回值很重要，没有返回值的化等于没有递归
 
 
 
