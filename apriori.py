02434e07d314ef4ab5844951340a608d8d8719fd
Mrhairui <chenwentao_622@163.com>
2019-06-14 22:44:04 +0800

    PERFECT

--- apriori.py
+++ apriori.py
@@ -30,7 +30,7 @@ def scan(dataset, Ck, minsupport):
     retlist = []
     supportdata = {}
     for key in ss.keys():
-        if ss[key] / length < minsupport:
+        if ss[key] / length >= minsupport:
             retlist.append(key)
         supportdata[key] = ss[key] / length
     return retlist, supportdata
@@ -40,14 +40,14 @@ def apriorigen(Lk, k):  # 两个集合求合并
     lenlk = len(Lk)
     retlist = []
     for i in range(lenlk):
-        for j in range(i, lenlk):
-            L1 = list(Lk[i])[:, k-2]
-            L2 = list(Lk[i])[:, k-2]
+        for j in range(i+1, lenlk):
+            L1 = list(Lk[i])[:k-2]
+            L2 = list(Lk[i])[:k-2]
             L1.sort()
             L2.sort()
             if L1 == L2:
                 retlist.append(Lk[i] | Lk[j])
-            return retlist
+    return retlist
 
 def apriori(dataset, minsupport):
     c1 = createC1(dataset)
@@ -57,14 +57,14 @@ def apriori(dataset, minsupport):
     while(len(L[k-2]) > 0):
         Ck = apriorigen(L[k-2], k)
         Lk, supk = scan(dataset, Ck, minsupport)
-        supportdata.updata(supk)   # updata 相当于列表里面的append
+        supportdata.update(supk)   # updata 相当于列表里面的append
         L.append(Lk)
         k+=1
-        return L, supportdata
+    return L, supportdata
 
 
 dataset = loadDataSet()
-L,supportdata = apriori(dataset, 0.7)
+L,supportdata = apriori(dataset, 0.5)
 
 
 
